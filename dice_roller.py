fac350c91e96359ad6971f48e312c7d597ce297c
Rudamentary99 <nathan.mccloskey@icloud.com>
2020-07-18 22:37:19 -0500

    added dynamic dice rolls

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,15 @@
-def main():
-  print('You rolled a die')
+import random
 
-if __name__== "__main__":
-  main()
+
+def main():
+    dice_amnt = 2
+    dice_sum = 0
+    for i in range(0, dice_amnt):
+        roll = random.randint(1, 20)
+        dice_sum += roll
+        print(f'You rolled a {roll}')
+    print(f'rolls total: {dice_sum}')
+
+
+if __name__ == "__main__":
+    main()
