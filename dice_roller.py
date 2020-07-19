d1f6c45bd09dd7fcb35ae1a70efcd26b4f0205ad
Rudamentary99 <nathan.mccloskey@icloud.com>
2020-07-18 22:55:55 -0500

    added input

--- dice_roller.py
+++ dice_roller.py
@@ -1,14 +1,29 @@
 import random
 
+dice_amnt = int(input('how many dice you tossing?'))
+dice_size = int(input('how many sides?'))
+
+
+def getResponse(pRoll):
+    try:
+        responses = {1: "you rolled a 1. critical fail",
+                     dice_size: "you rolled a critical success"}
+        return responses[pRoll]
+    except KeyError:
+        return ''
+
 
 def main():
-    dice_amnt = 2
     dice_sum = 0
     for i in range(0, dice_amnt):
-        roll = random.randint(1, 20)
+        roll = random.randint(1, dice_size)
         dice_sum += roll
-        print(f'You rolled a {roll}')
-    print(f'rolls total: {dice_sum}')
+        resp = getResponse(roll)
+        if resp:
+            print(resp)
+        else:
+            print(f'You rolled a {roll}')
+    print(f'\nrolls total: {dice_sum}')
 
 
 if __name__ == "__main__":
